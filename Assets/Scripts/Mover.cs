37edc0620a13bc5246a0d8514e8f956c1c241e77
agconti <agc11d@gmail.com>
2015-11-10 17:19:11 -0500

    feat(bolt): created bolt game object

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	
+	Rigidbody bolt;
+	[SerializeField] float boltSpeed;
+	
+	void Start () {
+		bolt = GetComponent<Rigidbody> ();
+		bolt.velocity = transform.forward * boltSpeed;
+	}
+}
