37fda760d0eb1f41b217b7efbd5be8f57cc48310
agconti <agc11d@gmail.com>
2015-11-11 13:55:03 -0500

    feat(explosions): added exposions for player and hazzards

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
+
+	[SerializeField] GameObject asteroidExplosion;
+	[SerializeField] GameObject playerExplosion;
 	
 	void OnTriggerEnter (Collider other) {
 
@@ -9,9 +12,12 @@ public class DestroyByContact : MonoBehaviour {
 			return; 
 		}
 
-		// Destroy bolt
+		if (other.CompareTag ("Player")) {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation); 
+		}
+
+		Instantiate (asteroidExplosion, transform.position, transform.rotation);
 		Destroy (other.gameObject);
-		// Destroy Asteroid
 		Destroy (gameObject);
 	}
 }
