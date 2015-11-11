cfb50547afafeb6ace141a7fc012ff4b833481c2
agconti <agc11d@gmail.com>
2015-11-11 14:42:48 -0500

    feat(waves): spawned waves by coroutine

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+	[SerializeField] float lifeTime;
+
+	void Start () {
+		Destroy (gameObject, lifeTime);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,18 +3,31 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	[SerializeField] GameObject asteroid; 
+	[SerializeField] GameObject asteroid;
+	[SerializeField] int hazardCount;
+	[SerializeField] float startWait;
 	[SerializeField] Vector3 spawnValues;
+	[SerializeField] float spawnWait;
+	[SerializeField] float waveWait;
 
 	void Start () {
 		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
 	
-	void  SpawnWaves () {
-		float xSpawnPosition = Random.Range (-spawnValues.x, spawnValues.x);
-		Vector3 spawnPosition = new Vector3(xSpawnPosition, spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (asteroid, spawnPosition, spawnRotation);
+	IEnumerator  SpawnWaves () {
+		yield return new WaitForSeconds (startWait);
 
+		while(true) {
+			for (int i = 0; i < hazardCount; i++) {
+				float xSpawnPosition = Random.Range (-spawnValues.x, spawnValues.x);
+				Vector3 spawnPosition = new Vector3(xSpawnPosition, spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+
+				Instantiate (asteroid, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			} 
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
