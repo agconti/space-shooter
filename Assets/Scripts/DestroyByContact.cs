1e467b1d9ecdb649dde8c3e87292d89897964f83
agconti <agc11d@gmail.com>
2015-11-11 13:41:47 -0500

    feat(asteroid): allowed players to destory asteroids

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	
+	void OnTriggerEnter (Collider other) {
+
+		if (other.CompareTag("Boundary")) {
+			return; 
+		}
+
+		// Destroy bolt
+		Destroy (other.gameObject);
+		// Destroy Asteroid
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	[SerializeField] float tumble;
+	Rigidbody asteroid;
+
+	void Start() {
+		asteroid = GetComponent<Rigidbody> ();
+		asteroid.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+}
